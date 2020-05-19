51591f0e537c5991c21e3a8b66b439fb2ac37b85
KSentak <k.sentak@outlook.com>
2020-05-19 13:34:14 -0400

    add front end to register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-// Import Components
+// Components
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -37,6 +37,58 @@ class Register extends Component {
         <h1>Register</h1>
 
         {/* Must build form */}
+        <div className='col-md-6 mt-5 mx-auto'>
+          <form noValidate onSubmit={this.onSubmit}>
+            <h1 className='h3 mb-3 font-weight-normal'>Please Register</h1>
+            <div className='form-group'>
+              <label htmlFor='first_name'>First Name</label>
+              <input
+                type='text'
+                className='form-control'
+                name='first_name'
+                placeholder='First Name'
+                value={this.state.first_name}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='last_name'>Last Name</label>
+              <input
+                type='text'
+                className='form-control'
+                name='last_name'
+                placeholder='Last Name'
+                value={this.state.last_name}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='email'>Email Address</label>
+              <input
+                type='email'
+                className='form-control'
+                name='email'
+                placeholder='Email Address'
+                value={this.state.email}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                className='form-control'
+                name='password'
+                placeholder='Password'
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+            </div>
+            <button type='submit' className='button btn-block'>
+              Register
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
