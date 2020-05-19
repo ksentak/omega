f1371d5c2209e239b4722c7d2916e969ed1aa76c
KSentak <k.sentak@outlook.com>
2020-05-19 13:36:46 -0400

    add backend logic to login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
+import { login } from '../../utils/UserFunctions';
 
 // CSS
 import './Login.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    login(user).then((res) => {
+      if (res) {
+        this.props.history.push('/profile');
+      }
+    });
+  }
+
   render() {
     return (
       <div className='Login'>
