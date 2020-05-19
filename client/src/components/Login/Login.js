3eb820b8e222b468a7a240c3920bfe478a4c1090
KSentak <k.sentak@outlook.com>
2020-05-19 13:38:10 -0400

    add front end to login component

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -39,6 +39,36 @@ class Login extends Component {
     return (
       <div className='Login'>
         <h1>Login</h1>
+        <div className='col-md-6 mt-5 mx-auto'>
+          <form noValidate onSubmit={this.onSubmit}>
+            <h1 className='h3 mb-3 font-weight-normal'>Please Log In</h1>
+            <div className='form-group'>
+              <label htmlFor='email'>Email Address</label>
+              <input
+                type='email'
+                className='form-control'
+                name='email'
+                placeholder='Email Address'
+                value={this.state.email}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className='form-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                className='form-control'
+                name='password'
+                placeholder='Password'
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+            </div>
+            <button type='submit' className='button btn-block'>
+              Sign In
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
