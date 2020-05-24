dad7e6699e7e4392933b09ba8eb3f2b388baf94e
KSentak <k.sentak@outlook.com>
2020-05-24 00:06:42 -0400

    add routing to landing page

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// CSS
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+// CSS
 import './index.css';
 
 ReactDOM.render(
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+// Images
+import logo from '../../assets/images/logo.png';
 
 // Material UI
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './LandingStyles';
 
-// Images
-import logo from '../../assets/images/logo.png';
-
 class Landing extends Component {
   render() {
     const { classes } = this.props;
+
     return (
       <div className='Landing'>
         <div className='container-fluid text-center'>
           <img src={logo} alt='logo' className={classes.logoImg} />
           <div classsName='row'>
-            <Button
-              className={classes.landingBtn}
-              variant='outlined'
-              size='large'
-            >
-              Login
-            </Button>
-            <Button
-              className={classes.landingBtn}
-              variant='outlined'
-              size='large'
-            >
-              Sign up
-            </Button>
+            {/* Log in */}
+            <Link to='/login'>
+              <Button
+                className={classes.landingBtn}
+                variant='outlined'
+                size='large'
+              >
+                Log in
+              </Button>
+            </Link>
+            {/* Register */}
+            <Link to='register'>
+              <Button
+                className={classes.landingBtn}
+                variant='outlined'
+                size='large'
+              >
+                Sign up
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
